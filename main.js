a6bde0e09c701858413dc842a1d5c94d51642bed
Sugiyama Yoshiyuki <yo.sugiyama67@gmail.com>
2023-09-30 00:53:47 +0900

    base update.

--- main.js
+++ main.js
@@ -10,60 +10,128 @@ const map = new mapboxgl.Map({
   container: 'map',
   style: 'mapbox://styles/mapbox/streets-v11',
   center: [tg_lon, tg_lat],
-  zoom: 14,
-  pitch: 60,
-  bearing: 20
+  zoom: 14.5,
+  pitch: 45,
+  bearing: -17.6,
+  antialias: true
 });
 
 map.on('load', () => {
-  // 洪水想定区域レイヤーの追加
-  map.addLayer({
-    'id': 'flood-hazard',
-    'type': 'raster',
-    'source': {
-      'type': 'raster',
-      'tiles': ['https://disaportaldata.gsi.go.jp/raster/01_flood_l1_shinsuishin_newlegend_data/{z}/{x}/{y}.png'],
-      'tileSize': 256,
-    },
-    'paint': { 'raster-opacity': 0.6 },
-    'layout': { 'visibility': 'visible' },
+
+  map.addControl(new mapboxgl.NavigationControl(), "bottom-right");
+
+  add3DBuildings();
+
+  // メニューからレイヤーをトグルするためのリスナーを設定
+  document.getElementById('flood-hazard-toggle').addEventListener('change', (event) => {
+    if (event.target.checked) {
+        addFloodLayer();
+    } else {
+        removeFloodLayer();
+    }
   });
 
-  // 標高レイヤーを追加(raster形式)
-  map.addLayer({
-    'id': 'elevation',
-    'type': 'hillshade',
-    'source': {
-      'type': 'raster-dem',
-      'url': 'mapbox://mapbox.terrain-rgb'
-    },
-    'layout': { 'visibility': 'visible' },
+  document.getElementById('elevation-toggle').addEventListener('change', (event) => {
+    if (event.target.checked) {
+        addElevationLayer();
+    } else {
+        removeElevationLayer();
+    }
   });
-
-  // ラジオコントロールでレイヤーの表示を切り替え
-  // const controls = document.querySelectorAll("input[name='layer']");
-  // controls.forEach((control) => {
-  //   control.addEventListener('change', (e) => {
-  //     let value = e.target.value;
-  //     if (value === 'flood') {
-  //       map.setLayoutProperty('flood-hazard', 'visibility', 'visible');
-  //       map.setLauoutProperty('elevation', 'visibility', 'none');
-  //     } else if (value === 'elevation') {
-  //       map.setLayoutProperty('flood-hazard', 'visibility', 'none');
-  //       map.setLayoutProperty('elevation', 'visibility', 'visible');
-  //     } else {
-  //       map.setLayoutProperty('flood-hazard', 'visibility', 'none');
-  //       map.setLayoutProperty('elevation', 'visibility', 'none');
-  //     }
-  //   });
-  // });
 });
 
-// 解説
-// 洪水想定区域レイヤーの追加: 指定されたURLを使用して、洪水想定区域のレイヤーをrasterタイプとして追加します。
+// 3D建物データを追加
+function add3DBuildings() {
+  map.addLayer({
+      'id': '3d-buildings',
+      'source': 'composite',
+      'source-layer': 'building',
+      'filter': ['==', 'extrude', 'true'],
+      'type': 'fill-extrusion',
+      'minzoom': 14,
+      'paint': {
+          'fill-extrusion-color': '#aaa',
+          'fill-extrusion-height': ['get', 'height'],
+          'fill-extrusion-base': ['get', 'min_height'],
+          'fill-extrusion-opacity': .6
+      }
+  });
+}
 
-// 標高レイヤーの追加: Mapboxのデフォルトのterrain-rgbデータセットを使用して、標高情報のレイヤーを追加します。これはhillshadeタイプのレイヤーとして追加されます。
+// layers.flood-hazard-3d: layer "flood-hazard-3d" requires a vector source
+// layers.flood-hazard-3d.paint.fill-extrusion-height[1][0]: Unknown expression "raster-data". If you wanted a literal array, use ["literal", [...]].
+// TODO ysugiyam ラスター形式では実現できないため
+// function addFloodLayer() {
+//   if (!map.getLayer('flood-hazard-3d')) {
+//       map.addLayer({
+//           'id': 'flood-hazard-3d',
+//           'type': 'fill-extrusion',
+//           'source': {
+//               'type': 'raster',
+//               'tiles': ['https://disaportaldata.gsi.go.jp/raster/01_flood_l1_shinsuishin_newlegend_data/{z}/{x}/{y}.png'],
+//               'tileSize': 256
+//           },
+//           'layout': {},
+//           'paint': {
+//               'fill-extrusion-color': '#088',
+//               'fill-extrusion-height': ['*', ['raster-data', ['get', 'source'], ['literal', [0, 0]]], 10],
+//               'fill-extrusion-opacity': 0.6
+//           }
+//       });
+//   }
+// }
 
-// レイヤーの切り替え: ページ上部にラジオボタンを追加して、ユーザーが表示するレイヤーを選択できるようにします。選択に応じて、対応するレイヤーのvisibilityプロパティを変更することでレイヤーの表示を切り替えます。
+// function removeFloodLayer() {
+//   if (map.getLayer('flood-hazard-3d')) {
+//       map.removeLayer('flood-hazard-3d');
+//   }
+// }
 
-// 標高データとしては、Mapboxのterrain-rgbデータセットを推奨します。このデータセットは全球的に利用可能で、高解像度の標高データを提供しています。
+function addFloodLayer() {
+  if (!map.getLayer('flood-hazard')) {
+      map.addLayer({
+          'id': 'flood-hazard',
+          'type': 'raster',
+          'source': {
+              'type': 'raster',
+              'tiles': ['https://disaportaldata.gsi.go.jp/raster/01_flood_l1_shinsuishin_newlegend_data/{z}/{x}/{y}.png'],
+              'tileSize': 256
+          },
+          'paint': { 'raster-opacity': 0.6 },
+      });
+  }
+}
+
+function removeFloodLayer() {
+  if (map.getLayer('flood-hazard')) {
+      map.removeLayer('flood-hazard');
+  }
+}
+
+function addElevationLayer() {
+
+  if (!map.getSource('mapbox-dem')) { // Sourceの存在を確認
+    map.addSource('mapbox-dem', {
+        'type': 'raster-dem',
+        'url': 'mapbox://mapbox.mapbox-terrain-dem-v1',
+        'tileSize': 512,
+        'maxzoom': 14
+    });
+  }
+
+  if (!map.getLayer('hillshading')) {
+      map.setTerrain({ 'source': 'mapbox-dem', 'exaggeration': 1.5 });
+      map.addLayer({
+          'id': 'hillshading',
+          'source': 'mapbox-dem',
+          'type': 'hillshade'
+      }, '3d-buildings');
+  }
+}
+
+function removeElevationLayer() {
+  if (map.getLayer('hillshading')) {
+      map.removeLayer('hillshading');
+      map.setTerrain(null);
+  }
+}
